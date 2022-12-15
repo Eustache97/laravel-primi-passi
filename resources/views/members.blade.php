fed6311f5dbeeb201051e9163520995ab1105ff8
Eustache97 <eubomber97@gmail.com>
2022-12-15 12:10:56 +0100

    multipage

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -5,13 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <title>Document</title>
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
 </head>
 
 <body>
@@ -23,8 +17,7 @@
         </ul>
     </header>
     <main>
-        <h1>HelloWord</h1>
-        <h3>This is class{{ $classNumber }} and I'm {{ $studentName }}</h3>
+        <h1>Here my class mates</h1>
     </main>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,14 @@ Route::get('/', function () {
     $classNumber = 73;
     $studentName = "Eustache";
     return view('home', compact("classNumber", "studentName"));
-});
+})->name("home");
+
+Route::get('/about', function () {
+    $pageTitle = "About us";
+    return view('about', compact("pageTitle"));
+})->name("about");
+
+Route::get('/members', function () {
+    $pageTitle = "Here my class mates";
+    return view('members', compact("pageTitle"));
+})->name("members");
